2ae09f89af891086a9afad827d2f2a3d481e6afa
BryanTaco <bstaco@puce.edu.ec>
2026-05-16 09:16:09 -0500

    Lista de Estudiantes

--- clase_4.kt
+++ clase_4.kt
@@ -32,5 +32,31 @@ fun main (){
         "Inactivo"
     }
     println("El estudiante ${student.name} esta $status")
+
+    val student2 = Student(
+    id = 2,
+    name = "Juan",
+    email = "juan@gmail.com",
+    grade = 5,
+    isActive = true
+    )
+    
+    val student3 = Student(
+    id = 3,
+    name = "Joaquin",
+    email = "joaquin@gmail.com",
+    grade = 10,
+    isActive = true
+    )
+    
+    val students = listOf(student, student2, student3)
+    println(students)
+    
+    for (student in students){
+        if (student.isActive){
+            println("${student.name} - ${student.grade}")
+        }
+    }
+}
     
 }
